d2e4fa2c450f72508222b401b893c22c2b6be0d5
Varun Reddy <varun.g@stayzilla.com>
2017-02-19 23:05:49 +0530

    Get documents in response

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
 var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	util = require('./util');
+  app = express(),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  util = require('./util')
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-var port = 3001;
-var cfg = {
-	'port' : 3001,
-	'ngram_index_path'   : __dirname + '/tmp/ngram_index.json',
-	'token_index_path'   : __dirname + '/tmp/token_index.json',
-	'stopwords_path'     : __dirname + '/tmp/stopwords.json',
-	'term_frequency_map' : __dirname + '/tmp/term_frequency_map.json',
-	'docs_path' : __dirname + '/docs/'
+const cfg = {
+  'port': 3001,
+  'ngram_index_path': path.join(__dirname, 'tmp/ngram_index.json'),
+  'token_index_path': path.join(__dirname, 'tmp/token_index.json'),
+  'stopwords_path': path.join(__dirname, 'tmp/stopwords.json'),
+  'term_frequency_map': path.join(__dirname, 'tmp/term_frequency_map.json'),
+  'docs_path': path.join(__dirname, 'docs/')
 }
 
 app.get('/', function (req, res) {
-	res.json('Hello Wanderer! I am Bolt :)')	
-});
+  res.json('Hello Wanderer! I am Bolt :)')
+})
 
 app.post('/index', function (req, res, next) {
-	if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('title')) {
-		console.log('Validation error');
-	}
-	util.indexFile(cfg, req.body, function (err, success) {
-		if (err != null) {
-			console.log(err);
-		}
-		console.log(success);
-		res.json(success);
-	});
-});
+  if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('title')) {
+    console.log('Validation error')
+  }
+  util.indexFile(cfg, req.body, function (err, success) {
+    if (err != null) {
+      console.log(err)
+    }
+    console.log(success)
+    res.json(success)
+  })
+})
 
 app.use('/search', (req, res, next) => {
-	var query_tokens = util.tokenize(req.query.q)
-	console.log(query_tokens);
-	console.log(process.index);
-	return util.getRelevantDocuments(query_tokens)
-	.then((documents) => {
-		res.json({
-			'success' : true,
-			'documents' : documents
-		});
-	}).catch((err) => {
-		console.log(err);
-	})
-});
+  var queryTokens = util.tokenize(req.query.q)
+  console.log(queryTokens)
+  console.log(process.index)
+  return util.getRelevantDocuments(queryTokens).then((documents) => {
+    return util.createPromises(documents)
+  }).then((data) => {
+    console.log(data)
+    res.json({
+      'success': true,
+      'documents': data
+    })
+  }).catch((err) => {
+    console.log(err)
+  })
+})
 
-
-util.readJsonFile(cfg.token_index_path).then((json_index) => {
-	process.index = json_index;
-	console.log("json_index " + json_index);
-	return util.readJsonFile(cfg.stopwords_path, [])
+util.createRequiredPaths(cfg)
+util.readJsonFile(cfg.token_index_path).then((jsonIndex) => {
+  process.index = jsonIndex
+  console.log('jsonIndex ' + jsonIndex)
+  return util.readJsonFile(cfg.stopwords_path, [])
 }).then((stopwords) => {
-	process.stopwords = stopwords;
-	console.log("stopwords " + process.stopwords);
-	return util.readJsonFile(cfg.term_frequency_map)
-}).then((term_frequency_map) => {
-	process.term_frequency_map = term_frequency_map;
-	console.log(term_frequency_map);
-	app.listen(cfg.port, function () {
-		console.log('Listening on port '+ cfg.port);
-	})
+  process.stopwords = stopwords
+  console.log('stopwords ' + process.stopwords)
+  return util.readJsonFile(cfg.term_frequency_map)
+}).then((termFrequencyMap) => {
+  process.term_frequency_map = termFrequencyMap
+  console.log(termFrequencyMap)
+  app.listen(cfg.port, function () {
+    console.log('Listening on port ' + cfg.port)
+  })
 })
 .catch((err) => {
-	console.log(err);
-	throw new Error(err);
+  console.log(err)
+  throw new Error(err)
 })
--- util.js
+++ util.js
@@ -1,188 +1,228 @@
-var async = require('async'),
-	fs = require('fs'),
-	path = require('path'),
-	_ = require('lodash');
+var fs = require('fs'),
+  path = require('path'),
+  _ = require('lodash')
 
-var configPath = '/tmp'
-function preprocessor(query) {
+// function preprocessor (query) {
+//   strippedString = query.replace(/\s/g, '')
+//   strippedString = strippedString.toLowerCase()
+// console.log(strippedString);
+//   ngrams = []
+//   len = strippedString.length
+//   for (start = 0; start < len; start++) {
+//     for (step = 1; step <= len - start; step++) {
+//       ngrams.push(strippedString.substr(start, step))
+//     }
+//   }
+//   return ngrams
+// }
 
-	strippedString = query.replace(/\s/g, "");
-	strippedString = strippedString.toLowerCase();
-	// console.log(strippedString);
-	ngrams = [];
-	len = strippedString.length;
-	for (start=0; start<len; start++) {
-		for (step =1; step<=len-start; step ++) {
-			ngrams.push(strippedString.substr(start, step));
-		}
-	}
-	return ngrams;
+function tokenize (query) {
+  query = query.toLowerCase()
+  var tokens = query.split(/\s+/g)
+  return removeStopWords(tokens)
 }
 
-function tokenize(query) {
-
-	query = query.toLowerCase();
-	tokens = query.split(/\s+/g);
-	return removeStopWords(tokens);
+function addDocToIndex (tokens, filePath) {
+  return new Promise((resolve) => {
+    if (tokens.length !== 0) {
+      tokens.forEach((token) => {
+        if (process.index.hasOwnProperty(token)) {
+          process.index[token].push(filePath)
+        } else {
+          process.index[token] = [filePath]
+        }
+        process.index[token] = _.uniq(process.index[token])
+      })
+    }
+    resolve()
+  })
 }
 
-function addDocToIndex(tokens, file_path) {
-	return new Promise((resolve) => {
-		if (tokens.length != 0) {
-			tokens.forEach((token) => {
-				if(process.index.hasOwnProperty(token)) {
-					process.index[token].push(file_path);
-				} else {
-					process.index[token] = [file_path];
-				}
-				process.index[token] = _.uniq(process.index[token])
-			})
-		}
-		resolve()
-	})
+function getFilePath (configPath, id) {
+  return `${configPath}${id}.json`
 }
 
-function getFilePath(configPath, id) {
-
-	return `${configPath}${id}.json`;
+function openFile (filePath, fallbackMode) {
+  return new Promise((resolve) => {
+    fs.open(filePath, 'wx+', (err, descriptor) => {
+      if (err != null) {
+        if (err.code === 'EEXIST') {
+          fs.open(filePath, fallbackMode, (err, fallbackDescriptor) => {
+            if (err != null) {
+              throw new Error()
+            }
+            resolve(fallbackDescriptor)
+          })
+          return
+        } else {
+          throw new Error(err)
+        }
+      }
+      resolve(descriptor)
+    })
+  })
 }
 
-function openFile(file_path) {
-	return new Promise((resolve) => {
-		fs.open(file_path, 'w', function (err, descriptor) {
-			if (err != null) {
-				throw new Error(err);
-			}
-			resolve(descriptor);
-		})
-	})
+function writeDocument (jsonData, filePath) {
+  return new Promise((resolve) => {
+    openFile(filePath, 'w').then((descriptor) => {
+      fs.write(descriptor, JSON.stringify(jsonData, null, 2), function (err) {
+        if (err) {
+          throw new Error(err)
+        }
+        resolve()
+      })
+    }).catch((err) => {
+      throw new Error(err)
+    })
+  })
 }
 
-function writeDocument(json_data, file_path) {
-	return new Promise((resolve) => {
-		openFile(file_path).then((descriptor) => {
-			fs.write(descriptor, JSON.stringify(json_data, null, 2), function (err) {
-				if (err) {
-					throw new Error(err);
-				}
-				resolve();
-			})
-		}).catch((err) => {
-			throw new Error(err);
-		})
-	})
-} 
-
-function indexFile(cfg, req_body, cb) {
-	// const ngrams = preprocessor(req_body.text);
-	const tokens = tokenize(req_body.text);
-	const doc_path = getFilePath(cfg.docs_path, req_body.id);
-	const doc_id = doc_path.split('/').slice(-1)[0];
-	writeDocument(req_body, doc_path)
-	.then(() => {
-		return addDocToIndex(tokens, doc_id)
-	})
-	.then(() => {
-		return storeTermFrequency(tokens, cfg.term_frequency_map, doc_id)
-	})
-	.then(() => {
-		console.log(process.index);
-		return writeDocument(process.index, cfg.token_index_path)
-	})
-	.then(() => {
-		resp = {
-			'success': true
-		}
-		cb(null, resp)
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+function indexFile (cfg, reqBody, cb) {
+  // const ngrams = preprocessor(reqBody.text);
+  const tokens = tokenize(reqBody.text)
+  const docPath = getFilePath(cfg.docs_path, reqBody.id)
+  const docId = docPath.split('/').slice(-1)[0]
+  if (process.term_frequency_map.hasOwnProperty(docId)) {
+    var resp = {
+      'success': false,
+      'error': 'A document already exists with that given docId. Try again with a different Id'
+    }
+    return cb(null, resp)
+  }
+  writeDocument(reqBody, docPath).then(() => {
+    return addDocToIndex(tokens, docId)
+  }).then(() => {
+    return storeTermFrequency(tokens, cfg.term_frequency_map, docId)
+  }).then(() => {
+    console.log(process.index)
+    return writeDocument(process.index, cfg.token_index_path)
+  }).then(() => {
+    var resp = {
+      'success': true
+    }
+    cb(null, resp)
+  }).catch((err) => {
+    console.error(err)
+  })
 }
 
-function readJsonFile(file_path, default_value= {}) {
-
-	return new Promise((resolve) => {
-		fs.readFile(file_path, function (err, json_string) {
-			var json_data;
-			if (err!= null) {
-				throw new Error(err);
-			}
-			try {
-				json_data = JSON.parse(json_string);
-			} catch(err) {
-				json_data = default_value;
-			}
-			resolve(json_data);
-		})
-	})
+function readJsonFile (filePath, defaultValue = {}) {
+  return new Promise((resolve) => {
+    openFile(filePath, 'r').then((fileDescriptor) => {
+      fs.readFile(fileDescriptor, function (err, jsonString) {
+        var jsonData
+        if (err != null) {
+          throw new Error(err)
+        }
+        try {
+          jsonData = JSON.parse(jsonString)
+        } catch (err) {
+          jsonData = defaultValue
+        }
+        resolve(jsonData)
+      })
+    }).catch((err) => {
+      throw new Error(err)
+    })
+  })
 }
 
-function removeStopWords(tokens) {
-	tokens.forEach((token, index) => {
-		if (process.stopwords.indexOf(token) > -1) {
-			tokens.splice(index, 1);
-		}
-	})
-	return tokens;
+function removeStopWords (tokens) {
+  tokens.forEach((token, index) => {
+    if (process.stopwords.indexOf(token) > -1) {
+      tokens.splice(index, 1)
+    }
+  })
+  return tokens
 }
 
-function getRelevantDocuments(tokens) {
-
-	return new Promise((resolve) => {
-		var documents = [];
-		tokens.forEach((token) => {
-			if (process.index.hasOwnProperty(token)) {
-				documents = documents.concat(process.index[token]);
-			}
-		})
-		documents = _.uniq(documents);
-		console.log(documents);
-		selected_docs_tf = scoreDocumentsByTF(tokens, documents);
-		resolve(selected_docs_tf);
-	});
+function getRelevantDocuments (tokens) {
+  return new Promise((resolve) => {
+    var documents = []
+    tokens.forEach((token) => {
+      if (process.index.hasOwnProperty(token)) {
+        documents = documents.concat(process.index[token])
+      }
+    })
+    documents = _.uniq(documents)
+    console.log(documents)
+    var selectedDocsTf = scoreDocumentsByTF(tokens, documents)
+    resolve(selectedDocsTf)
+  })
 }
 
-function scoreDocumentsByTF(tokens, documents) {
-	selected_docs_tf = [];
-	console.log("Inside Scoring");
-	console.log(tokens);
-	console.log(documents);
-	console.log(process.term_frequency_map);
-	documents.forEach((doc) => {
-		// console.log(process.term_frequency_map[doc]);
-		tf_score = 0;
-		tokens.forEach((token) => {
-			if (process.term_frequency_map[doc].hasOwnProperty(token)) {
-				console.log(doc, token, Math.log10(process.term_frequency_map[doc][token]+1));
-				tf_score += Math.log10(process.term_frequency_map[doc][token]+1);
-			}
-		});
-		selected_docs_tf.push({'doc': doc, tf_score: tf_score});
-	});
-	console.log(selected_docs_tf);
-	return _.sortBy(selected_docs_tf, [(doc) => {return (1/ (1+ doc.tf_score));}]);
+function scoreDocumentsByTF (tokens, documents) {
+  var selectedDocsTf = []
+  console.log('Inside Scoring')
+  console.log(tokens)
+  console.log(documents)
+  console.log(process.term_frequency_map)
+  documents.forEach((doc) => {
+// console.log(process.term_frequency_map[doc]);
+    var tfScore = 0
+    tokens.forEach((token) => {
+      if (process.term_frequency_map[doc].hasOwnProperty(token)) {
+        console.log(doc, token, Math.log10(process.term_frequency_map[doc][token] + 1))
+        tfScore += Math.log10(process.term_frequency_map[doc][token] + 1)
+      }
+    })
+    selectedDocsTf.push({'doc': doc, tfScore: tfScore})
+  })
+  console.log(selectedDocsTf)
+  return _.sortBy(selectedDocsTf, [(doc) => { return (1 / (1 + doc.tfScore)) }])
 }
 
-function storeTermFrequency(tokens, term_frequency_path, doc_id) {
-	return new Promise((resolve) => {
-		tf_map = {};
-		tokens.forEach((token) => {
-			if (tf_map.hasOwnProperty(token)) {
-				tf_map[token] += 1
-			} else {
-				tf_map[token] = 1;
-			}
-		});
-		process.term_frequency_map[doc_id] = tf_map;
-		console.log(process.term_frequency_map);
-		return writeDocument(process.term_frequency_map, term_frequency_path).then(() => resolve())
-	})
+function storeTermFrequency (tokens, termFrequencyPath, docId) {
+  return new Promise((resolve) => {
+    var tfMap = {}
+    tokens.forEach((token) => {
+      if (tfMap.hasOwnProperty(token)) {
+        tfMap[token] += 1
+      } else {
+        tfMap[token] = 1
+      }
+    })
+    process.term_frequency_map[docId] = tfMap
+    console.log(process.term_frequency_map)
+    return writeDocument(process.term_frequency_map, termFrequencyPath).then(() => resolve())
+  })
+}
+
+function createRequiredPaths (cfg) {
+  var reqPaths = ['docs']
+  reqPaths.forEach((p) => {
+    var fullPath = path.join(__dirname, p)
+    if (!fs.existsSync(fullPath)) {
+      fs.mkdirSync(fullPath)
+    }
+  })
+}
+
+function promisifyRead (filePath) {
+  return new Promise((resolve) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err != null) {
+        throw new Error(err)
+      }
+      resolve(JSON.parse(data))
+    })
+  })
+}
+
+function createPromises (documents) {
+  var promises = []
+  documents.forEach((d) => {
+    promises.push(promisifyRead(path.join(__dirname, 'docs', d.doc)))
+  })
+  return Promise.all(promises)
 }
 
 module.exports = {
-	indexFile : indexFile,
-	readJsonFile : readJsonFile,
-	tokenize : tokenize,
-	getRelevantDocuments : getRelevantDocuments
+  indexFile: indexFile,
+  readJsonFile: readJsonFile,
+  tokenize: tokenize,
+  getRelevantDocuments: getRelevantDocuments,
+  createRequiredPaths: createRequiredPaths,
+  createPromises: createPromises
 }
